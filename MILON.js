2a498aef29ec626a404719570fccc69372d3ad70
Milom-BoT-60 <milonbot0@gmail.com>
2026-01-26 10:22:46 +0600

    Update and rename IMRAN.js to MILON.js

--- MILON.js
+++ MILON.js
@@ -1,5 +1,5 @@
 module.exports = async ({ api, event }) => {
-  const logger = require('./main/catalogs/IMRANC.js')
+  const logger = require('./main/catalogs/MILONC.js')
   
   const configCustom = {
     autosetbio: {
