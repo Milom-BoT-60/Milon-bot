9585ba2e0dea08438332bd43e57aa8f08d867179
Milom-BoT-60 <milonbot0@gmail.com>
2026-01-26 11:04:24 +0600

    Update pp.js

--- scripts/commands/pp.js
+++ scripts/commands/pp.js
@@ -1,33 +1,53 @@
 module.exports.config = {
-  name: "pp",
-  version: "2.0.0",
-  permission: 0,
-  credits: "Milon Fix (No Token)",
-  prefix: true,
-  description: "Send Facebook profile picture without token",
-  category: "image"
+ name: "pp",
+ version: "1.0.0",
+ hasPermssion: 0,
+ credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ description: "Lấy ID người dùng.",
+ commandCategory: "Công cụ",
+ cooldowns: 0
 };
 
-module.exports.run = async function ({ api, event, args }) {
-  let uid;
-
-  if (event.type === "message_reply") {
-    uid = event.messageReply.senderID;
-  } else if (Object.keys(event.mentions).length > 0) {
-    uid = Object.keys(event.mentions)[0];
-  } else if (args[0]) {
-    uid = args[0];
-  } else {
-    uid = event.senderID;
-  }
-
-  const imgURL = `https://graph.facebook.com/${uid}/picture?type=large`;
-
-  return api.sendMessage(
-    {
-      body: "🖼️ Profile Picture",
-      attachment: await global.utils.getStreamFromURL(imgURL)
-    },
-    event.threadID
-  );
-};
+module.exports.run = async function({ event, api, args, client, Currencies, Users, utils, __GLOBAL, reminder }) {
+const fs = global.nodemodule["fs-extra"];
+ const request = global.nodemodule["request"];
+ const axios = global.nodemodule['axios']; 
+ if(event.type == "message_reply") { 
+ let name = await Users.getNameUser(event.messageReply.senderID) 
+	uid = event.messageReply.senderID
+	var callback = () => api.sendMessage({body:`==profile==━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+ () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+ return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+ () => callback()); 
+ }
+ if (!args[0]) {
+ var uid = event.senderID;
+ 
+ var callback = () => api.sendMessage({body:`==profile==━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+ () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+ return request(encodeURI(`https://graph.facebook.com/${event.senderID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+ () => callback()); 
+ }
+ else {
+	if (args[0].indexOf(".com/")!==-1) {
+ const res_ID = await api.getUID(args[0]);
+ var name = data.name
+var data = await api.getUserInfoV2(res_ID);
+ var username = data.username
+ var link = data.link
+ var callback = () => api.sendMessage({body:`=== [ profile==]`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+ () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+ return request(encodeURI(`https://graph.facebook.com/${res_ID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+ () => callback()); }
+	else {
+		if (args.join().indexOf('@') !== -1) 
+ var uid = Object.keys(event.mentions) 
+ var callback = () => 
+api.sendMessage({body:`=== profile===`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+ () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+ return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+ () => callback()); 
+ 
+	}
+}
+}
